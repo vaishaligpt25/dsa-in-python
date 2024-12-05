ec492ad8be68739a56487febbc4d5c520c0c8fd1
vaishaligpt25 <vaishali.gpt25@gmail.com>
2024-12-05 10:52:23 +0530

    leetcode/maths/leetcode_231_power_of_two.py

--- leetcode/maths/leetcode_231_power_of_two.py
+++ leetcode/maths/leetcode_231_power_of_two.py
@@ -1,10 +1,7 @@
 class Solution:
     def isPowerOfTwo(self, n: int) -> bool:
-        if n == 0:
+        if n <= 0:
             return False
-
-        if n < 0:
-            n = n * (-1)
 
         while n > 1:
             if n % 2 != 0:
