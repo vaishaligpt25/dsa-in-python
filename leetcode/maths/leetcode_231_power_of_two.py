ca370ad947ca8bf83aec68b5e19e30791655ca3c
vaishaligpt25 <vaishali.gpt25@gmail.com>
2024-12-23 17:33:59 +0000

    modify leetcode-231-power-of-two

--- leetcode/maths/leetcode_231_power_of_two.py
+++ leetcode/maths/leetcode_231_power_of_two.py
@@ -1,10 +1,28 @@
-class Solution:
-    def isPowerOfTwo(self, n: int) -> bool:
-        if n <= 0:
-            return False
+def PowerOfTwo(n: int) -> bool:
+    if n <= 0:
+        return False
 
-        while n > 1:
-            if n % 2 != 0:
-                return False
-            n = n / 2
-        return True
+    while n > 1:
+        if n % 2 != 0:
+            return False
+        n = n / 2
+    return True
+
+
+def output_PowerOfTwo(n: int) -> None:
+    if PowerOfTwo(n=n):
+        print(f"number {n} is power of 2")
+    else:
+        print(f"number {n} is NOT power of 2")
+
+
+if __name__ == '__main__':
+    output_PowerOfTwo(16)
+    output_PowerOfTwo(25)
+    output_PowerOfTwo(300)
+    output_PowerOfTwo(400)
+    output_PowerOfTwo(1)
+    output_PowerOfTwo(2)
+    output_PowerOfTwo(4)
+    output_PowerOfTwo(8)
+    output_PowerOfTwo(1024)
