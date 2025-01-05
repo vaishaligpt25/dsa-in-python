e24376839cdd4e712de9cc05ae2dbdb0b52ed64a
vaishaligpt25 <vaishali.gpt25@gmail.com>
2025-01-05 17:02:40 +0000

    Number_of_burgers_with_no_waste_of_ingredients

--- leetcode/maths/leetcode-1276-number-of-burgers-with-no-waste-of-ingredients.py
+++ leetcode/maths/leetcode-1276-number-of-burgers-with-no-waste-of-ingredients.py
@@ -1,45 +1,65 @@
-# LeetCode-1276: https://leetcode.com/problems/number-of-burgers-with-no-waste-of-ingredients/
-
-"""
-By creating and solving simultaneous linear equations relating
-1. t: no of tomato slices
-2. c: no of cheese slices
-3. j: no of jumbo burgers
-4. s: no of small burgers
-
-we get solutions
-(a) j = (t - 2c) / 2
-(b) s = (4c - t) / 2
-
-so we can proceed as follows
-1. calculate j & s using above equations (rounded down to nearest integers)
-  - also use basic heuristics for early return such as j & s can't be negative
-2. verify that above equations hold true (since we rounded down the results obtained from them)
-  - if verification succeeds, return [j, s]
-  - otherwise return []
-"""
+# leetcode_1276:- https://leetcode.com/problems/number-of-burgers-with-no-waste-of-ingredients
 
 from typing import List
 
 
 class Solution:
     def numOfBurgers(self, tomatoSlices: int, cheeseSlices: int) -> List[int]:
-        if max(tomatoSlices, cheeseSlices) == 0:
-            return [0, 0]
+        return self._num_of_jumbo_and_small_burger(num_of_cheeseSlices=cheeseSlices, num_of_tomatoSlices=tomatoSlices)
 
-        num_jumbo_burgers: int = int((tomatoSlices - (2 * cheeseSlices)) / 2)
-        if num_jumbo_burgers < 0:
+    def _num_of_jumbo_and_small_burger(self, num_of_tomatoSlices: int, num_of_cheeseSlices: int) -> List[int]:
+        total_num_of_jumbo_burger: int = int((num_of_tomatoSlices - (2 * num_of_cheeseSlices)) / 2)
+        if total_num_of_jumbo_burger < 0:
             return []
 
-        num_small_burgers: int = int(((4 * cheeseSlices) - tomatoSlices) / 2)
-        if num_small_burgers < 0:
+        total_num_of_small_burger: int = int(((4 * num_of_cheeseSlices) - num_of_tomatoSlices) / 2)
+        if total_num_of_small_burger < 0:
             return []
 
-        # verify results
-        expected_num_tomato_slices: int = (4 * num_jumbo_burgers) + (2 * num_small_burgers)
-        expected_num_cheese_slices: int = num_jumbo_burgers + num_small_burgers
-
-        if (tomatoSlices != expected_num_tomato_slices) or (cheeseSlices != expected_num_cheese_slices):
+        condition_1_check: bool = (total_num_of_jumbo_burger + total_num_of_small_burger) == num_of_cheeseSlices
+        condition_2_check: bool = (4* total_num_of_jumbo_burger + 2* total_num_of_small_burger) == num_of_tomatoSlices
+        if condition_1_check and condition_2_check:
+            return [total_num_of_jumbo_burger, total_num_of_small_burger]
+        else:
             return []
 
-        return [num_jumbo_burgers, num_small_burgers]
+
+
+
+
+
+
+
+class SolutionTest:
+    def test__num_of_jumbo_and_small_burger(self) -> None:
+        soln: Solution = Solution()
+        num_of_tomatoSlices_in: int
+        num_of_cheeseSlices_in: int
+        result_out_expected: List[int]
+        result_out_computed: List[int]
+
+        num_of_tomatoSlices_in = 16
+        num_of_cheeseSlices_in = 7
+        result_out_expected = [1, 6]
+        result_out_computed = soln._num_of_jumbo_and_small_burger(num_of_tomatoSlices=num_of_tomatoSlices_in,
+                                                                  num_of_cheeseSlices=num_of_cheeseSlices_in)
+        assert result_out_expected == result_out_computed
+
+        num_of_tomatoSlices_in = 17
+        num_of_cheeseSlices_in = 4
+        result_out_expected = []
+        result_out_computed = soln._num_of_jumbo_and_small_burger(num_of_tomatoSlices=num_of_tomatoSlices_in,
+                                                                  num_of_cheeseSlices=num_of_cheeseSlices_in)
+        assert result_out_expected == result_out_computed
+
+        num_of_tomatoSlices_in = 4
+        num_of_cheeseSlices_in = 17
+        result_out_expected = []
+        result_out_computed = soln._num_of_jumbo_and_small_burger(num_of_tomatoSlices=num_of_tomatoSlices_in,
+                                                                  num_of_cheeseSlices=num_of_cheeseSlices_in)
+        assert result_out_expected == result_out_computed
+
+
+if __name__ == '__main__':
+    soln_test: SolutionTest = SolutionTest()
+    soln_test.test__num_of_jumbo_and_small_burger()
