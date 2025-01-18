5337a639760178a14a4d880f68ffa97ef06268a5
vaishaligpt25 <vaishali.gpt25@gmail.com>
2025-01-18 18:40:10 +0000

    leetcode_13_roman_to_integer

---
+++ leetcode/maths/leetcode_13_roman_to_integer.py
@@ -0,0 +1,14 @@
+# leetcode_13:-https://leetcode.com/problems/roman-to-integer
+
+# class Solution:
+#     def romanToInt(self, s: str) -> int:
+#
+#
+#      def creat_dict(self,key: str, value: int) -> None:
+#          dict_list = {}
+key = ("I", "V", "X", "L", "C", "D", "M")
+value = (1, 5, 10, 50, 100, 500, 1000)
+dict_list = dict(zip(key, value))
+print(dict_list)
+
+
