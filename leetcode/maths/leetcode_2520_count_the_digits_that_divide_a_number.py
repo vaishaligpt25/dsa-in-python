e1467244f1c390afbbb06d5906fe965e1f42df3f
vaishaligpt25 <vaishali.gpt25@gmail.com>
2025-01-26 13:25:00 +0000

    leetcode_2520_count_the_digits_that_divide_a_number

--- leetcode/maths/leetcode_2520_count_the_digits_that_divide_a_number.py
+++ leetcode/maths/leetcode_2520_count_the_digits_that_divide_a_number.py
@@ -4,7 +4,7 @@ class Solution:
     def countDigits(self, num: int) -> int:
         # store original number since we will modify temp
         total_count: int = 0
-        temp: int= num
+        temp: int = num
 
         # Extract each digit and check if it divides num
         while temp > 0:
@@ -14,13 +14,15 @@ class Solution:
             temp //= 10
         return total_count
 
+
 class SolutionTest:
     def countDigits(self) -> None:
         soln: Solution = Solution()
         assert soln.countDigits(num=7) == 1
-        assert soln.countDigits(num= 121) == 2
+        assert soln.countDigits(num=121) == 2
         assert soln.countDigits(num=0) == 0
         assert soln.countDigits(num=1000) == 1
+
 
 if __name__ == '__main__':
     soln_test: SolutionTest = SolutionTest()
