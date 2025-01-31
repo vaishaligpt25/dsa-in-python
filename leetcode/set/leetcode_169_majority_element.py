12cd376fee589e4f82fb98b0f735a8718efcd0d7
vaishaligpt25 <vaishali.gpt25@gmail.com>
2025-01-31 06:24:57 +0000

    leetcode_169_majority_element

---
+++ leetcode/set/leetcode_169_majority_element.py
@@ -0,0 +1,6 @@
+#leetcode_169https://leetcode.com/problems/majority-element/description/?envType=problem-list-v2&envId=array
+
+from typing import List, Set,
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
