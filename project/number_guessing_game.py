c88577bf10409a7a3d37ae40e23029ed4390138f
vaishaligpt25 <vaishali.gpt25@gmail.com>
2025-04-09 17:56:33 +0000

    number_guessing_game

--- project/number_guessing_game.py
+++ project/number_guessing_game.py
@@ -3,18 +3,15 @@
 import random
 
 number_to_guess = random.randint(1, 100)
-
-try:
-    guess = int(input('Guess the number between 1 and 100: '))
-    if guess < number_to_guess:
-        print('Too Low!')
-    elif guess > number_to_guess:
-        print('Too High!')
-    else:
-        print('Congratulations! Guessd the correct number')
-except ValueError:
-    print('Please enter a valid number')
-
-
-
-
+while True:
+    try:
+        guess = int(input('Guess the number between 1 and 100: '))
+        if guess < number_to_guess:
+            print('Too Low!')
+        elif guess > number_to_guess:
+            print('Too High!')
+        else:
+            print('Congratulations! Guessed the correct number')
+            break
+    except ValueError:
+        print('Please enter a valid number')
