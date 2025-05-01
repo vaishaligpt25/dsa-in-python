e6bc207fc0fdda909e13549522cd0d3c0fe72f0f
vaishaligpt25 <vaishali.gpt25@gmail.com>
2025-05-01 09:06:59 +0000

    leetcode_2399_check_distances_between_same_letters

---
+++ leetcode/string/leetcode_2399_check_distances_between_same_letters.py
@@ -0,0 +1,8 @@
+# leetcode_2399_https://leetcode.com/problems/check-distances-between-same-letters
+from typing import List
+
+
+class Solution:
+    def checkDistances(self, s: str, distance: List[int]) -> bool:
+
+
