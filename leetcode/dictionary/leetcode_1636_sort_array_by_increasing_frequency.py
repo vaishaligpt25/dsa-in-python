6d137219165c05da52c454098d3a2093d9857f2a
vaishaligpt25 <vaishali.gpt25@gmail.com>
2025-05-03 15:33:16 +0000

    leetcode_1636_sort_array_by_increasing_frequency

--- leetcode/dictionary/leetcode_1636_sort_array_by_increasing_frequency.py
+++ leetcode/dictionary/leetcode_1636_sort_array_by_increasing_frequency.py
@@ -9,3 +9,14 @@ class Solution:
             freq[num] = freq.get(num, 0) + 1
         return sorted(nums, key=lambda x: (freq[x], -x))
 
+class SolutionTest:
+    def check_frequencySort(self) -> None:
+        soln: Solution = Solution()
+        assert soln.frequencySort(nums = [1,1,2,2,2,3]) == [3,1,1,2,2,2]
+        assert soln.frequencySort(nums = [2,3,1,3,2]) == [1,3,3,2,2]
+        assert soln.frequencySort(nums = [-1,1,-6,4,5,-6,1,4,1]) == [5,-1,4,4,-6,-6,1,1,1]
+
+
+if __name__ == '__main__':
+    soln_test: SolutionTest = SolutionTest()
+    soln_test.check_frequencySort()
