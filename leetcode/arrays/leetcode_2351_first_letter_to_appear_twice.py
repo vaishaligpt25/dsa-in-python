d680b108257d966529c515a352f2e183feda9779
vaishaligpt25 <vaishali.gpt25@gmail.com>
2025-05-15 10:38:19 +0000

    leetcode_2351_first_letter_to_appear_twice

---
+++ leetcode/arrays/leetcode_2351_first_letter_to_appear_twice.py
@@ -0,0 +1,32 @@
+# leetcode_2351:https://leetcode.com/problems/first-letter-to-appear-twice
+
+
+class Solution:
+    def repeatedCharacter(self, s: str) -> str:
+        s_map = {}
+        for char in s:
+            if char in s_map:
+                return char
+            s_map[char] = 1
+        return ""
+
+    def repeatedCharacter1(self, s: str) -> str:
+       seen = set()
+       for char in s:
+           if char in seen:
+               return char
+           seen.add(char)
+       return ""
+
+
+class SolutionTest:
+    def check_repeatedCharacter(self) -> None:
+        soln: Solution = Solution()
+        assert soln.repeatedCharacter(s="abccbaacz") == "c"
+        assert soln.repeatedCharacter(s="abcdd") == "d"
+
+
+
+
+
+
