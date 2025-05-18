b5e529716423697f915f83ad603be117b76fe12c
vaishaligpt25 <vaishali.gpt25@gmail.com>
2025-05-18 06:49:49 +0000

    Use env-vars & params dict

--- project/weather_program.py
+++ project/weather_program.py
@@ -1,10 +1,24 @@
 # https://www.youtube.com/watch?v=SqvVm3QiQVk&t=1494s
+import os
+
+from typing import Dict, Any
 
 import requests
 from pprint import pprint
 
-API_Key = "e0129df9e8e60df521cbd86f0e091a9c"
-city = input("Enter a city: ")
-base_url = "http://api.openweathermap.org./data/2.5/weather?appid="+API_Key+"&q="+city
-weather_data = requests.get(base_url).json()
+api_key: str = os.getenv("API_KEY")
+city: str = input("Enter a city: ")
+
+# base_url: str = "http://api.openweathermap.org./data/2.5/weather?appid="+API_Key+"&q="+city
+# weather_data = requests.get(base_url).json()
+
+base_url: str = "http://api.openweathermap.org/data/2.5/weather"
+params: Dict[str, str] = {
+    "q": city,
+    "appid": api_key
+}
+
+response: requests.models.Response = requests.get(base_url, params=params)
+weather_data: Dict[str, Any] = response.json()
+
 pprint(weather_data)
